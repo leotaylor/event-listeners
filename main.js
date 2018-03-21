9ba98eca48ba61fb199c543fa646ecb18354771a
Leo Taylor <leotaylor723@gmail.com>
2018-03-20 21:10:51 -0500

    looking good

--- main.js
+++ main.js
@@ -54,3 +54,22 @@ const students = [
         catchPhrase: "Multi-channelled solution-oriented artificial intelligence"
     }
 ];
+
+const printToDom = (domString, divId) => {
+    const printTo = document.getElementById(divId);
+    printTo.innerHTML = domString;
+};
+
+const buildDromString = (studentArray) => {
+    let domString = '';
+    studentArray.forEach((student) => {
+        domString += `<div class="card">`;
+        domString +=    `<h1>${student.firstName} ${student.lastName}</h1>`;
+        domString +=    `<h3>${student.catchPhrase}</h3>`;
+        domString +=    `<img src="${student.avatar}" alt="">`;
+        domString +=    `<button class="button">Brought Pie</button>`;
+        domString +=    `</div>`;
+    });
+    printToDom(domString, 'card-holder');
+}
+buildDromString(students);
